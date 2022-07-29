afec3bfdf7df9f2e34e05ac6b3f797257e64fa79
ranyery <ranyerysc@gmail.com>
2022-07-28 23:07:17 -0300

    refactor: ajusta funcao extrairLInks para retornar objeto chave-valor

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function getFile(filePath) {
   const encoding = "utf8";
   try {
     const content = await promises.readFile(filePath, encoding);
-    console.log(chalk.green(extractLinks(content)));
+    console.log(extractLinks(content));
   } catch (e) {
     handleError(e);
   }
@@ -15,7 +15,12 @@ async function getFile(filePath) {
 
 function extractLinks(content) {
   const regEx = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
-  const extractedLinks = content.match(regEx);
+  const extractedLinks = [];
+  let temp;
+  while ((temp = regEx.exec(content)) != null) {
+    const occurrence = { [temp[1]]: temp[2] };
+    extractedLinks.push(occurrence);
+  }
   return extractedLinks;
 }
 
